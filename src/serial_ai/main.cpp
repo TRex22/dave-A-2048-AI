ac437f23a6c9f85a3711a93cf98256aec59af986
jason chalom <jchalom@mscluster.ms.wits.ac.za>
2017-05-04 18:27:23 +0200

    started stack stuff, incomplete something serious came up, will finish tomorrow

--- src/serial_ai/main.cpp
+++ src/serial_ai/main.cpp
@@ -26,17 +26,12 @@
 #include <string.h>
 #include <math.h>
 
-#include "../tree/tree.h"
+#include "../tree_builder/tree_builder.cpp"
 
 const int board_size = 4;
 
 int main(int argc, char *argv[]);
-bool checkAtRoot(Node* node);
-bool canContinue(Node* node);
-bool isLeaf(Node* node);
-void buildTree(Tree* tree);
-void findChildren(Tree* tree, Node& node);
-bool findCurrentNodeFromChildren(Tree* tree, Node& node);
+
 
 int main(int argc, char *argv[])
 {
@@ -52,138 +47,4 @@ int main(int argc, char *argv[])
 
 }
 
-bool checkAtRoot(Node* node)
-{
-	if (node->depth == 0)
-		return true;
 
-	return false;
-}
-
-bool canContinue(Node* node)
-{
-	bool won = determine_2048(node->current_state);
-
-	if(!checkAtRoot(node) && !won)
-		return true;
-
-	if(node->test == false)
-		return true;
-
-	return false;
-}
-
-bool isLeaf(Node* node)
-{
-	GameState *state_left;
-	GameState *state_right;
-	GameState *state_up;
-	GameState *state_down;
-
-	GameState* states[] = {state_left, state_right, state_up, state_down};
-
-	for (int i = 0; i < 4; ++i)
-	{
-		states[i] = new GameState(board_size);
-		states[i]->copy(node->current_state);
-		process_action(states[i], i);
-		if ( node->current_state->equals(states[i]) )
-		{
-			return false;
-		}
-	}
-
-	return true;
-}
-
-//todo fix this shit
-bool findCurrentNodeFromChildren(Tree* tree, Node& node)
-{
-	// printf("%d, %d\n", node->children[0]->test, node->children[1]->test);
-	if(node.children[0]->test == false)
-	{
-		// findChildren(tree, *node.children[0]);
-		node = *(node.children[0]);
-		return true;
-	}
-	else if(node.children[1]->test == false)
-	{
-		// findChildren(tree, *node.children[1]);
-		node = *(node.children[1]);
-		return true;
-	}
-	else if(node.children[2]->test == false)
-	{
-		// findChildren(tree, *node.children[2]);
-		node = *(node.children[2]);
-		return true;
-	}
-	else if(node.children[3]->test == false)
-	{
-		// findChildren(tree, *node.children[3]);
-		node = *(node.children[3]);
-		return true;
-	}
-
-	return false;
-}
-
-/*NOTE: Should only create a new children if not a leaf node ... meaning need to check if leaf, not root, etc*/
-
-void buildTree(Tree* tree)
-{
-	Node* root = tree->root;
-	Node* current_node = root;
-	Node* parent_node = current_node->parent;
-	Node* previous_node = current_node;
-
-	while(canContinue(current_node))
-	{
-		findChildren(tree, *current_node);
-
-		previous_node = current_node;
-		parent_node = current_node->parent;
-		bool check = findCurrentNodeFromChildren(tree, *current_node);
-
-		if(compare_game_states(current_node->current_state, previous_node->current_state))
-		{
-			while(!check && !checkAtRoot(current_node))
-			{
-				current_node = parent_node;
-				check = findCurrentNodeFromChildren(tree, *current_node);
-				parent_node = parent_node->parent;
-			}
-		}
-	}
-}
-
-void findChildren(Tree* tree, Node& node)
-{
-	GameState *state_left;
-	GameState *state_right;
-	GameState *state_up;
-	GameState *state_down;
-
-	GameState* states[] = {state_left, state_right, state_up, state_down};
-
-	for (int i = 0; i < 4; ++i)
-	{
-		states[i] = new GameState(board_size);
-		states[i]->copy(node.current_state);
-		process_action(states[i], i);
-		add_new_number(states[i]);
-
-		int currentDepth = node.depth + 1;
-		if(tree->max_depth < currentDepth)
-			tree->max_depth = currentDepth;
-		
-		node.children[i] = new Node(node, states[i], currentDepth);
-
-		tree->num_nodes++;
-	}
-
-	node.test = true;
-
-	printf("%d, %d, %d\n", tree->num_nodes, tree->max_depth, determine_highest_value(node.current_state));
-	print_board(node.current_state);
-}
--- src/tree_builder/tree_builder.cpp
+++ src/tree_builder/tree_builder.cpp
@@ -1,1 +1,101 @@
 #include <stack>
+
+#include "../tree/tree.h"
+
+using namespace std;
+
+Tree* buildTree(Tree* tree);
+void generateChidlren(Node* currentNode, stack<Node> tracker);
+bool checkAtRoot(Node* node);
+bool canContinue(Node* node);
+bool isLeaf(Node* node);
+
+Tree* buildTree(Tree* tree)
+{
+	stack<Node> tracker;
+	tracker.push(tree.root);
+
+	while(!tracker.empty())
+	{
+		int board_size = 4; //TODO: make as parameter to do this properly
+		Node* currentNode = tracker.pop();
+
+		if(!leaf)
+		{
+			generateChidlren(currentNode, tracker, board_size);
+		}
+
+	}
+}
+
+void generateChidlren(Node* currentNode, stack<Node> tracker, int board_size)
+{
+	GameState *state_left;
+	GameState *state_right;
+	GameState *state_up;
+	GameState *state_down;
+
+	GameState* states[] = {state_left, state_right, state_up, state_down};
+
+	for (int i = 3; i >= 0; --i)
+	{
+		states[i] = new GameState(board_size);
+		states[i]->copy(currentNode.current_state);
+		process_action(states[i], i);
+		add_new_number(states[i]);
+
+		int currentDepth = node.depth + 1;
+		if(tree->max_depth < currentDepth)
+			tree->max_depth = currentDepth;
+		
+		currentNode.children[i] = new Node(currentNode, states[i], currentDepth);
+
+		tree->num_nodes++;
+	}
+
+	currentNode.test = true;
+}
+
+bool checkAtRoot(Node* node)
+{
+	if (node->depth == 0)
+		return true;
+
+	return false;
+}
+
+bool canContinue(Node* node)
+{
+	bool won = determine_2048(node->current_state);
+
+	if(!checkAtRoot(node) && !won)
+		return true;
+
+	if(node->test == false)
+		return true;
+
+	return false;
+}
+
+bool isLeaf(Node* node)
+{
+	GameState *state_left;
+	GameState *state_right;
+	GameState *state_up;
+	GameState *state_down;
+
+	GameState* states[] = {state_left, state_right, state_up, state_down};
+
+	for (int i = 0; i < 4; ++i)
+	{
+		states[i] = new GameState(board_size);
+		states[i]->copy(node->current_state);
+		process_action(states[i], i);
+		if ( node->current_state->equals(states[i]) )
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
