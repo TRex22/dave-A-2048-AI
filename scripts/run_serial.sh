ee08cdfca6133c16ac19eb5d931c7315c0601726
Liron Mizrahi <lmizrahi12@gmail.com>
2017-05-18 02:21:54 +0200

    Added start of demo

--- scripts/run_serial.sh
+++ scripts/run_serial.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./bin/serial_ai.out --max_num_nodes=10000 --print_output --print_path --use_rnd
+./bin/serial_ai.out --max_num_nodes=10000 --save_to_file --save_csv --print_output --print_path --use_rnd
